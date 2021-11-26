8e6311296168089bf954b8c40738d13e6616a20a
Bart Budz <bart.budz@gmail.com>
2021-11-25 22:37:00 -0600

    Got the pixel example to work... Jesus...

--- intro/exercise-i-8/Walker.js
+++ intro/exercise-i-8/Walker.js
@@ -7,15 +7,36 @@ class Walker {
         this.ty = 10000;
     }
 
-    update() {
-        this.x = map(noise(this.tx), 0, 1, 0, width);
-        this.y = map(noise(this.ty), 0, 1, 0, height);
+    setPixelColor(x, y) {
+        let d = pixelDensity();
 
-        this.tx += 0.01;
-        this.ty += 0.01;
+        for(let i = 0; i < d; i++) {
+            for (let j = 0; j < d; j++) {
+                let index = 4 * ((y * d + j) * width * d + (x * d + i));
+
+                let bright = random(255);
+                let c = color(bright);
+
+                pixels[index] = c._getRed();
+                pixels[index + 1] = c._getGreen();
+                pixels[index + 2] = c._getBlue();
+                pixels[index + 3] = c._getAlpha();
+            }
+        }
+    }
+
+    update() {
+        loadPixels();
+
+        for (let x = 0; x < width; x++) {
+            for (let y = 0; y < height; y++) {
+                this.setPixelColor(x, y);
+            }
+        }
+
+        updatePixels();
     }
 
     show() {
-        point(this.x, this.y);
     }
 }
