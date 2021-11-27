8298b46ef8f78b4017abf7c8d99bbc125a7da560
Bart Budz <bart.budz@gmail.com>
2021-11-26 21:17:04 -0600

    Restored previous code

--- intro/exercise-i-9/sketch.js
+++ intro/exercise-i-9/sketch.js
@@ -1,9 +1,15 @@
+let w;
+
 function setup() {
     let canvas = createCanvas(200, 200);
     canvas.parent("canvas");
 
     background("#dfdfdf");
+    
+    w = new Walker();
 }
 
 function draw() {
+    w.update();
+    w.show();
 }
