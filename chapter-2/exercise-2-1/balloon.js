2be9cf5704031adfae95f8f1fd2402d1b0780300
Bart Budz <bart.budz@gmail.com>
2022-01-15 22:29:12 -0600

    Ballon rises and bounces now

--- chapter-2/exercise-2-1/balloon.js
+++ chapter-2/exercise-2-1/balloon.js
@@ -1,58 +1,40 @@
 class Balloon {
     constructor() {
-        // dimensions of balloon
-        this.radius = 8;
-
         this.location = createVector(width / 2, height);
-        this.velocity = createVector(0, -0.75);
+        this.velocity = createVector(0, 0);
         this.acceleration = createVector(0, 0)
-        
-        this.wIncrement = 0;
     }
 
-    getWindForce() {
-        let xDirection = map(noise(this.wIncrement), 0, 1, -1, 1);
-        return createVector(xDirection, 0);
-    }
-
-    stopAcceleration() {
-        this.acceleration.mult(0);
-    }
-
-    stopVelocity() {
-        this.velocity.mult(0);
+    applyForce(force) {
+        this.acceleration.add(force);
     }
 
     checkEdges() {
-        // check if th eballoon hit the top of the window
-        // check y value - 2 times radius because of how the ballon is drawn.
-        if (this.location.y - this.radius * 2 <= 0) {
-            this.stopAcceleration();
-            this.stopVelocity();
+        if(this.location.y < 0) {
+            this.location.y = 0;
+            this.velocity.y *= -1;
+            this.velocity.div(2);
         }
-        
-        if (this.location.x <= 0) {
+
+        if (this.location.x < 0) {
             this.location.x = 0;
-        } else if (this.location.x >= width) {
+            this.velocity.x *= -1;
+        } else if (this.location.x > width) {
             this.location.x = width;
+            this.velocity.x *= -1;
         }
     }
 
     update() {
-        this.acceleration = this.getWindForce();
         this.velocity.add(this.acceleration);
         this.location.add(this.velocity);
 
-        // reset acceleration
-        this.stopAcceleration();
-        this.wIncrement += 2;
+        this.acceleration.mult(0);
     }
 
     display() {
         stroke(0);
         fill(175);
-
-        // make sure to take the radius into account
-        ellipse(this.location.x, this.location.y - this.radius, this.radius * 2, this.radius * 2);
+        ellipse(this.location.x, this.location.y, 16, 16);
     }
 }
--- chapter-2/exercise-2-1/sketch.js
+++ chapter-2/exercise-2-1/sketch.js
@@ -1,5 +1,10 @@
 var balloon;
 
+function getWindForce() {
+    let xDirection = map(noise(this.wIncrement), 0, 1, -1, 1);
+    return createVector(xDirection, 0);
+}
+
 function setup() {
     let canvas = createCanvas(800, 800);
     canvas.parent("canvas");
@@ -8,6 +13,9 @@ function setup() {
 
 function draw() {
     background('#dfdfdf');
+
+    let heliumRiseForce = createVector(0, -0.05);
+    balloon.applyForce(heliumRiseForce);
 
     balloon.update();
     balloon.checkEdges();
