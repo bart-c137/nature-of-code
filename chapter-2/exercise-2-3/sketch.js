0b856f81e1c596739653b795e871f9d06792436c
Bart Budz <bart.budz@gmail.com>
2022-03-12 23:49:03 -0600

    Implemented exponential decay to get the force

--- chapter-2/exercise-2-3/sketch.js
+++ chapter-2/exercise-2-3/sketch.js
@@ -1,12 +1,39 @@
 var movers;
-// var mover;
 var p;
+
+/**
+ * Returns the force value for a location based on how close it is to the specified edge dimension.
+ * 
+ * @param {number} location - The location to calculate the force for.
+ * @param {number} edge - The maximum value for the edge to check.
+ * @returns {number} A number representing the strength of the force to apply.
+ */
+function exponentailDecay(location, edge) {
+    let dNorm = 0;
+    let direction = 1;
+
+    if (location < edge / 2) {
+        // the location is to the left of the middle, the location is how close it os to the edge
+        dNorm = location;
+    } else {
+        // the location is to the right of the middle, calculate how close it is to the edge by subtracting from the
+        // edge value
+        dNorm = edge - location;
+
+        // the returning force must push to the left
+        direction = -1;
+    }
+
+    // using random values for the starting and decay values.
+    let force = 5 * Math.pow(1 - 0.025, dNorm)
+    return force * direction;
+}
 
 function setup() {
     let canvas = createCanvas(800, 800);
     canvas.parent("canvas");
+
     movers = new Array(100);
-    // mover = new Mover(random(0.1, 5), random(0, width), random(0, height));
 
     for (let i = 0; i < movers.length; i++) {
         movers[i] = new Mover(random(0.1, 5), random(0, width), random(0, height));
@@ -22,30 +49,14 @@ function draw() {
     let gravity = createVector(0, 0.1);
 
     for (let i = 0; i < movers.length; i++) {
-        let xNorm = movers[i].location.x / width;
-        let yNorm = movers[i].location.y / height;
-        let edgeForce = createVector(0.5 - xNorm, 0.5 - yNorm);
-        
+        let xForce = exponentailDecay(movers[i].location.x, width);
+        let yForce = exponentailDecay(movers[i].location.y, height);
+        let edgeForce = createVector(xForce, yForce);
+
         movers[i].applyForce(edgeForce);
         movers[i].applyForce(leftWind);
         movers[i].applyForce(gravity);
         movers[i].update();
         movers[i].display();
-        movers[i].checkEdges();
     }
-
-    // let xNorm = mover.location.x / width;
-    // let yNorm = mover.location.y / height;
-    // let edgeForce = createVector(0.5 - xNorm, 0.5 - yNorm);
-
-    // mover.applyForce(edgeForce);
-
-    // mover.applyForce(leftWind);
-    // mover.applyForce(gravity);
-
-    // mover.update();
-    // mover.display();
-    // mover.checkEdges();
-
-    // p.html("Velocity: " + mover.velocity.x + "<br />Distance: " + d);
 }
