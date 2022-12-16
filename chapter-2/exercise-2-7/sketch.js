49a644403c9b156d789bfcc9ca8fa069ceb9829f
Bart Budz <bart.budz@gmail.com>
2022-12-15 22:12:40 -0600

    Removed unnecessary code

--- chapter-2/exercise-2-7/sketch.js
+++ chapter-2/exercise-2-7/sketch.js
@@ -5,25 +5,10 @@ function setup() {
     canvas.parent("canvas");
 
     wing = new BoxMover(10, 0, height - 50, 100, 50)
-
-    p = select("#output");
 }
 
 function draw() {
     background('#dfdfdf');
-
-    let output = "";
-
-    // is the object in the liquid?
-    // if (wing.isInside(liquid)) {
-    //     let dragForce = wing.drag(liquid);
-
-    //     // is the current drag higher
-    //     if (dragForce.mag() > wing.maxDrag) {
-    //         wing.maxDrag = dragForce.mag();
-    //         wing.maxVelocity = wing.velocity.mag();
-    //     }
-    // }
 
     wing.fly();
     wing.lift();
@@ -35,9 +20,4 @@ function draw() {
     wing.update();
     wing.display();
     wing.checkEdges();
-
-    output += "Object " + (wing + 1) + " - Start Height: " + (height - wing.startY).toFixed(2) + ".  ";
-    output += "Max Drag: " + wing.maxDrag.toFixed(2) + ".  Max Speed: " + wing.maxVelocity.toFixed(2) + ".  ";
-    output += "Surface Area: " + wing.width + "<br />";
-    p.html(output);
 }
